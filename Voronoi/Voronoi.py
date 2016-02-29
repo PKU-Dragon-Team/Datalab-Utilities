e2c7ea6e09801a71e02c8eb91a4f519a3b747cb6
SLAPaper <slapaper.pku@gmail.com>
2016-02-29 18:16:17 +0800

    feature: use pandas to load data and modified plot format

--- Voronoi/Voronoi.py
+++ Voronoi/Voronoi.py
@@ -1,36 +1,37 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from scipy.spatial import Voronoi, voronoi_plot_2d
+import pandas
 
-import csv
+import os
 
 COUNT_LIMIT = None
-SAMPLE_LIMIT = 100
+SAMPLE_LIMIT = 20
 
-Points = []
-with open('cell_info.csv', 'r', encoding='utf_8') as obj_file:
-    csv_file = csv.reader(obj_file)
-    for cnt, line in enumerate(csv_file):
-        if COUNT_LIMIT and cnt >= COUNT_LIMIT:  # 只读取前 k 个点
-            break
-        Points.append([float(line[1]), float(line[2])])  # make up data points
+cell_info = pandas.read_csv('cell_info.csv', header=None, dtype={0: str, 1: float, 2:float}, nrows=COUNT_LIMIT)
+cell_info.columns = ("name", "x", "y")
 
-# 随机抽取 n 个点
+# 随机抽取
 if SAMPLE_LIMIT:
-    points = np.array([Points[i] for i in np.random.choice(len(Points), size = SAMPLE_LIMIT)])
+    cells = np.array([(cell_info["x"][i], cell_info["y"][i]) for i in np.random.choice(len(cell_info), size = SAMPLE_LIMIT)])
 else:
-    points = np.array(Points)
+    cells = np.array(cell_info["x", "y"])
 
 # compute Voronoi tesselation
-vor = Voronoi(points)
+vor = Voronoi(cells)
 
 # plot
-voronoi_plot_2d(vor)
+fig = voronoi_plot_2d(vor)
+fig.canvas.set_window_title("基站服务范围示意图")
+title = fig.suptitle("cell_info")
+ax = fig.gca()
+ax.set_aspect(1.)
 
 # colorize
 for region in vor.regions:
-    if not -1 in region:
+    if -1 not in region:
         polygon = [vor.vertices[i] for i in region]
-        plt.fill(*zip(*polygon))
+        _ = ax.fill(*zip(*polygon)) # plt.fill((x0, x1, x2, x3, ...), (y0, y1, y2, y3, ...), color=None)
 
 plt.show()
