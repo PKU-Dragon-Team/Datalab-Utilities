a551528673e6f918bacea403b8a7ebfb8c4b160a
SLAPaper <slapaper.pku@gmail.com>
2016-04-30 19:07:26 +0800

    refactor: take advantage of reshaped data

--- mobile_cluster/__init__.py
+++ mobile_cluster/__init__.py
@@ -14,7 +14,8 @@ import typing as tg
 import sys
 from pathlib import Path
 
-__location__ = os.path.join(os.getcwd(), os.path.dirname(os.path.realpath(__file__)))
+__location__ = os.path.join(os.getcwd(),
+                            os.path.dirname(os.path.realpath(__file__)))
 __path__ = Path(__location__)
 
 sys.path.append(str(__path__.parent))
@@ -45,22 +46,20 @@ def main() -> None:
         DATABASE = conf['database']
         CHARSET = conf['charset']
 
-    connection = pymysql.connect(host=HOST, user=USER, password=PASS, db=DATABASE, charset=CHARSET, cursorclass=pymysql.cursors.DictCursor)
+    connection = pymysql.connect(host=HOST,
+                                 user=USER,
+                                 password=PASS,
+                                 db=DATABASE,
+                                 charset=CHARSET,
+                                 cursorclass=pymysql.cursors.DictCursor)
 
-    sql = "SELECT location, day_type, shour, usr_count, x, y FROM `loc_week_count`"
+    sql = "SELECT usr_count_json, x, y FROM `loc_week_count_reshape`"
     data = pd.read_sql(sql, connection)
 
     dset = {}
     for i in range(len(data)):
         row = data.iloc[i]
-        try:
-            dset[(row.x, row.y)]
-        except KeyError:
-            dset[(row.x, row.y)] = [0] * 2 * 24
-        if row.day_type == "weekday":
-            dset[(row.x, row.y)][row.shour] = row.usr_count
-        elif row.day_type == "weekend":
-            dset[(row.x, row.y)][row.shour + 24] = row.usr_count
+        dset[(row.x, row.y)] = json.loads(row.usr_count_json)
 
     dframe = pd.DataFrame.from_dict(dset, orient='index')
     location = pd.DataFrame.from_records([row for row in dframe.index])
@@ -73,18 +72,26 @@ def main() -> None:
         result = kmeans.fit_predict(dframe)
         color = (result + 1) / (c + 1)
         rgb_color = [gray2rgb(x) for x in color]
-        handles = [mpatches.Patch(color=gray2rgb((i + 1) / (c + 1)), label='cluster %d' % (i + 1)) for i in range(c)]
+        handles = [mpatches.Patch(color=gray2rgb((i + 1) / (c + 1)),
+                                  label='cluster %d' % (i + 1))
+                   for i in range(c)]
         if i % 2 != 0:
             anchor = (-0.3, 0.5)
             loc = 7
         else:
             anchor = (1.05, 0.5)
             loc = 6
-        ax.legend(handles=handles, bbox_to_anchor=anchor, loc=loc, borderaxespad=0.0)
+        ax.legend(handles=handles,
+                  bbox_to_anchor=anchor,
+                  loc=loc,
+                  borderaxespad=0.0)
         ax.set_autoscale_on(False)
         ax.set_xlim(115.8, 116.9)
         ax.set_ylim(39.6, 40.3)
-        Voronoi.voronoi(location, color_set=pd.DataFrame.from_records(rgb_color), target_axes=ax, show=False)
+        Voronoi.voronoi(location,
+                        color_set=pd.DataFrame.from_records(rgb_color),
+                        target_axes=ax,
+                        show=False)
     plt.show()
 
     connection.close()
