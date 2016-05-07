3c1a1137ecf3235ae743492d8498399e1f761577
SLAPaper <slapaper.pku@gmail.com>
2016-05-07 20:59:14 +0800

    fix: remove dask and make it work

--- mobile_cluster/Cluster_determ.py
+++ mobile_cluster/Cluster_determ.py
@@ -2,47 +2,41 @@
 """
 
 import numpy as np
-import dask.array as da
 import typing as tg
 
+import math
 
-def method_1994(X: da.Array, Ra: float=1, Rb: float=1.5, epsilon_upper: float=0.5, epsilon_lower: float=0.15) -> tg.Tuple[tg.List[float], tg.List[int]]:
+
+def method_1994(X: np.matrix, Ra: float=0.5, Rb: float=0.75, epsilon_upper: float=0.5, epsilon_lower: float=0.15):
     """funtion that use the 1994 Fuzzy Model Identification Based on Cluster Estimation method
         return the index of Cluster point in X and the potential of them
     """
-
-    def cal_potential(xi: int, X: da.Array, alpha: float):
+    def cal_potential(xi: int, X: np.matrix, alpha: float):
         """Function calculate the potential of point x become a cluster center
         """
-        return da.exp(-alpha * (X[da.fromfunction(lambda x: x != xi, chunks=(1024, 1024), shape=(X.shape[0], ), dtype=bool), :] - X[xi, :])**2).sum()
-
-    def cal_new_potential(old_i: int, center_i: int, X: da.Array, p: da.Array, beta: float):
+        return np.exp(-alpha * ((X[np.fromfunction(lambda i: i != xi, shape=(X.shape[0], )), :] - X[xi, :])**2).sum(axis=1)).sum()
+    def cal_new_potential(old_i: int, center: tg.Tuple[np.double, int], X: np.matrix, p: np.ndarray, beta: float):
         """Function calculate the updated potential of point x after a new cluster center occurs
         """
-        return (-p[old_i] + p[center_i] * (-beta * da.linalg.lstsq(X[old_i], X[center_i]))**2)
-
-    def cal_d_min(xi: int, centers: tg.List[tg.Tuple[np.double, int]], X: da.Array):
+        return p[old_i] - center[0] * np.exp(-beta * ((X[old_i, :] - X[center[1], :])**2).sum())
+    def cal_d_min(xi: int, centers: tg.List[tg.Tuple[np.double, int]], X: np.matrix):
         """Function calculate the shortest distance between point X[xi, :] and all previous cluster centers
         """
-        return da.fromfunction(lambda i: da.linalg.lstsq(X[xi], X[i]), chunks=(1024, 1024), shape=(len(centers, )), dtype=np.double).min()
-
+        return math.sqrt(((X[tuple(zip(*centers))[1], :] - X[xi, :])**2).sum(axis=1).min())
     alpha = 4 / Ra**2
     beta = 4 / Rb**2
-
     # calc the first center
-    potential = da.fromfunction(lambda i: cal_potential(i, X, alpha), chunks=(1024, 1024), shape=(X.shape[0], ), dtype=np.double)
-    first_center_i = potential.argmax().compute()
+    potential = np.fromiter((cal_potential(i, X, alpha) for i in range(X.shape[0])), dtype=np.double)
+    first_center_i = potential.argmax()
     first_center_p = potential[first_center_i]
     centers = [(first_center_p, first_center_i), ]  # KDTree is not modifiable, so use plain method
     calculated_count = 1
-
-    potential = da.fromfunction(lambda i: cal_new_potential(i, first_center_i, X, potential, beta), chunks=(1024, 1024), shape=potential.shape, dtype=np.double)
-
+    print("Center %d detected." % len(centers))
+    potential = np.fromiter((cal_new_potential(i, (first_center_p, first_center_i), X, potential, beta) for i in range(X.shape[0])), dtype=np.double)
     while len(potential) > calculated_count:
         most_potential_i = potential.argmax()
         most_potential_p = potential[most_potential_i]
         calculated_count += 1
-
         while True:
             if most_potential_p > epsilon_upper * first_center_p:
                 accepted = True
@@ -50,18 +44,18 @@ def method_1994(X: da.Array, Ra: float=1, Rb: float=1.5, epsilon_upper: float=0.
             elif most_potential_p < epsilon_lower * first_center_p:
                 accepted = False
                 break
-            elif cal_d_min(X[most_potential_i, :], centers, X) / Ra + most_potential_p / first_center_p > 1:
+            elif cal_d_min(most_potential_i, centers, X) / Ra + most_potential_p / first_center_p > 1:
                 accepted = True
                 break
             else:
-                potential = da.fromfunction(lambda i: 0 if i == most_potential_i else potential[i], chunks=(1024, 1024), shape=potential.shape, dtype=np.double)  # da.Array is immutable
+                potential[most_potential_i] = 0
                 most_potential_i = potential.argmax()
                 most_potential_p = potential[most_potential_i]
                 calculated_count += 1
-
         if accepted:
             centers.append((most_potential_p, most_potential_i))
-            potential = da.fromfunction(lambda i: cal_new_potential(i, first_center_i, X, potential, beta), chunks=(1024, 1024), shape=potential.shape, dtype=np.double)
+            potential = np.fromiter((cal_new_potential(i, (most_potential_p, most_potential_i), X, potential, beta) for i in range(X.shape[0])), dtype=np.double)
+            print("Center %d detected." % len(centers))
         else:
             break
     return tuple(zip(*centers))
