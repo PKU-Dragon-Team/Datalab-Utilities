964dd4334df3eec634b8a634b09def920bc216d4
SLAPaper <slapaper.pku@gmail.com>
2016-05-07 20:58:48 +0800

    feat: add status display

--- mobile_cluster/calc_clustercenter.py
+++ mobile_cluster/calc_clustercenter.py
@@ -4,12 +4,12 @@
 import numpy as np
 import sklearn.preprocessing as sklpp
 import pandas as pd
-import dask.array as da
 import pymysql
 
 import os
 import json
 from pathlib import Path
+import time
 
 import Cluster_determ as cd
 
@@ -27,9 +27,12 @@ if __name__ == '__main__':
 
     connection = pymysql.connect(host=HOST, user=USER, password=PASS, db=DATABASE, charset=CHARSET, cursorclass=pymysql.cursors.DictCursor)
 
+    print("Loding data from SQL…")
     sql = "SELECT usr_count_json, x, y FROM `loc_week_count_reshape`"
     data = pd.read_sql(sql, connection)
+    print("Data loded.")
 
+    print("Building data structure…")
     dset = {}
     for i in range(len(data)):
         row = data.iloc[i]
@@ -38,12 +41,22 @@ if __name__ == '__main__':
     dframe = pd.DataFrame.from_dict(dset, orient='index')
     # location = pd.DataFrame.from_records([row for row in dframe.index])
     # location.columns = ("x", "y")
+    print("Data structure builded.")
 
-    X = da.from_array(sklpp.minmax_scale(np.matrix(dframe, dtype=np.double), copy=False), (1000, 1000))
+    print("Preprocessing data…")
+    X = sklpp.minmax_scale(np.matrix(dframe, dtype=np.double), copy=False)
+    print("Data preprocessed.")
 
+    print("Calculating cluster centers…")
+    t0 = time.time()
     result = cd.method_1994(X)
+    t1 = time.time()
+    print("Cluster centers alculated.")
+    print("Total time: %.2f" % (t1 - t0))
 
+    print("Dumping result…")
     with open(os.path.join(__location__, 'cluster_centers.json'), 'w', encoding='utf_8') as f:
         json.dump(result, f, indent=4)
+    print("Result dumped.")
 
     connection.close()
