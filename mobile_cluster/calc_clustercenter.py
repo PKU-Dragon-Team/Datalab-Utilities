2f94c398487444cefc2e6713c6ec72c6c7ff556f
SLAPaper <slapaper.pku@gmail.com>
2016-05-07 15:16:56 +0800

    refactor: remove unnessasary code

--- mobile_cluster/calc_clustercenter.py
+++ mobile_cluster/calc_clustercenter.py
@@ -9,32 +9,12 @@ import pymysql
 
 import os
 import json
-import decimal
-import typing as tg
-import sys
 from pathlib import Path
 
 import Cluster_determ as cd
 
 __location__ = os.path.join(os.getcwd(), os.path.dirname(os.path.realpath(__file__)))
 __path__ = Path(__location__)
-
-sys.path.append(str(__path__.parent))
-
-
-class DecimalEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, decimal.Decimal):
-            return str(obj)
-        return json.JSONEncoder.default(self, obj)
-
-
-def gray2rgb(grayscale: float) -> tg.Tuple:
-    """The function to change grayscale to an RGB tuple
-    the grayscale is requeired to be in range of [0, 1]
-    """
-    return (grayscale, grayscale, grayscale)
-
 
 if __name__ == '__main__':
     with open(os.path.join(__location__, "config.json"), 'r') as config:
