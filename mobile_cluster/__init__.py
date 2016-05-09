de66e2cf33a0c499576ccf38dfe7b63e73250e53
SLAPaper <slapaper.pku@gmail.com>
2016-05-09 22:02:36 +0800

    feat: use the output of affinity propagation to run fuzzy c-means

--- mobile_cluster/__init__.py
+++ mobile_cluster/__init__.py
@@ -91,39 +91,59 @@ def main() -> None:
     X_sample = X[sample_mask, :]
     print("Data preprocessed.")
 
-    # centers = np.matrix(X[cluster_centers, :])
-    # c = len(cluster_centers)
-    # m = fuzzifier_determ(X.shape[1], X.shape[0])
-    # kmeans = sklc.MiniBatchKMeans(n_clusters=c, init=centers, max_iter=1e8)
-    # fcmeans = skfc.cmeans(X, c, m, 1e-6, 10000, init=centers)
-    # result = kmeans.fit_predict(dframe)
-    print("Model fitting and sample predicting…")
+    print("Affinity propagation model fitting…")
     t0 = time.time()
-    ap = sklc.AffinityPropagation().fit(X_sample)
-    # result = ap.predict(X)
+    ap = sklc.AffinityPropagation(damping=.99, max_iter=10000).fit(X_sample)
     cluster_centers = ap.cluster_centers_indices_
     c = len(cluster_centers)
     t1 = time.time()
-    print("Model fitted and sample predicted.")
+    print("Affinity propagation model fitted.")
     print("%d cluster centers dectected." % c)
     print("Total time: %.2f" % (t1 - t0))
 
-    # print("Plotting result…")
-    # color = (result + 1) / (c + 1)
-    # rgb_color = [gray2rgb(x) for x in color]
-    # ax = plt.subplot(1, 1, 1)
-    # ax.set_autoscale_on(False)
-    # ax.set_xlim(115.8, 116.9)
-    # ax.set_ylim(39.6, 40.3)
-    # Voronoi.voronoi(location, color_set=pd.DataFrame.from_records(rgb_color), target_axes=ax, show=False)
-    # plt.show()
+    ap_result = ap.predict(X)
+    init_matrix = np.fromfunction(lambda c, n: ap_result[n] == c, (c, X.shape[0]), dtype=int)
+
+    # print("DBSCAN propagation model fitting…")
+    # t0 = time.time()
+    # ap = sklc.DBSCAN().fit(X)
+    # # result = ap.predict(X)
+    # cluster_centers = ap.core_sample_indices_
+    # c = len(cluster_centers)
+    # t1 = time.time()
+    # print("DBSCAN propagation model fitted.")
+    # print("%d cluster centers dectected." % c)
+    # print("Total time: %.2f" % (t1 - t0))
+
+    print("Fuzzy c-means model fitting…")
+    t2 = time.time()
+    # centers = np.matrix(X[sample_mask[cluster_centers, :]])
+    m = fuzzifier_determ(X.shape[1], X.shape[0])
+    fcm = {}
+    fcm['cntr'], fcm['u'], fcm['u0'], fcm['d'], fcm['jm'], fcm['p'], fcm['fpc'] = skfc.cmeans(X.T, c, m, 1e-3, 100, init_matrix)  # cmeans accept (D, N) rather than (N, D)
+    t3 = time.time()
+    print("Fuzzy c-means model fitted.")
+    print("Total time: %.2f" % (t3 - t2))
+    # kmeans = sklc.MiniBatchKMeans(n_clusters=c, init=centers, max_iter=1e8)
+    # result = kmeans.fit_predict(dframe)
+
+    print("Plotting result…")
+    result = fcm['u'].T.argmax(axis=1)
+    color = (result + 1) / (c + 1)
+    rgb_color = [gray2rgb(x) for x in color]
+    ax = plt.subplot(1, 1, 1)
+    ax.set_autoscale_on(False)
+    ax.set_xlim(115.8, 116.9)
+    ax.set_ylim(39.6, 40.3)
+    Voronoi.voronoi(location, color_set=pd.DataFrame.from_records(rgb_color), target_axes=ax, sample_limit=0, show=False)
+    plt.show()
     # handles = [mpatches.Patch(color=gray2rgb((i + 1) / (c + 1)), label='cluster %d' % (i + 1)) for i in range(c)]
     # ax.legend(handles=handles, loc=0, borderaxespad=0.0)
-    # print("Result plotted.")
+    print("Result plotted.")
 
-    print("Dumping result…")
-    with open(os.path.join(__location__, 'cluster_centers.json'), 'w', encoding='utf_8') as f:
-        json.dump(dframe.iloc[sample_mask[cluster_centers], :], f, cls=NumpyAndPandasEncoder)
-    print("Result dumped.")
+    # print("Dumping result…")
+    # with open(os.path.join(__location__, 'cluster_centers.json'), 'w', encoding='utf_8') as f:
+    #     json.dump(dframe.iloc[sample_mask[cluster_centers], :], f, cls=NumpyAndPandasEncoder)
+    # print("Result dumped.")
 
     connection.close()
