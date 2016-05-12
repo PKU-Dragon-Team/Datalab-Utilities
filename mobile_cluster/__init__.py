543fe077fc61a8b052d78e2bf27b7c2875289165
SLAPaper <slapaper.pku@gmail.com>
2016-05-13 01:34:55 +0800

    milestone: use Birch to calculate centers

--- mobile_cluster/__init__.py
+++ mobile_cluster/__init__.py
@@ -1,21 +1,22 @@
 """the package for clustering mobile_data
 """
-import numpy as np
-import pandas as pd
-import sklearn.cluster as sklc
-import skfuzzy.cluster as skfc
-import sklearn.preprocessing as sklpp
-import pymysql
-import matplotlib.pyplot as plt
-
-import os
 import json
 import math
-import typing as tg
+import numbers
+import os
 import sys
 import time
-import numbers
+import typing as tg
 from pathlib import Path
+import pdb
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pymysql
+import skfuzzy.cluster as skfc
+import sklearn.cluster as sklc
+import sklearn.preprocessing as sklpp
 
 __location__ = os.path.join(os.getcwd(), os.path.dirname(os.path.realpath(__file__)))
 __path__ = Path(__location__)
@@ -65,68 +66,62 @@ def main() -> None:
     connection = pymysql.connect(host=HOST, user=USER, password=PASS, db=DATABASE, charset=CHARSET, cursorclass=pymysql.cursors.DictCursor)
 
     print("Loding data from SQL…")
-    sql = "SELECT usr_count_json, x, y FROM `loc_week_count_reshape`"
-    data = pd.read_sql(sql, connection)
+    sql = "SELECT usr_count_normalize, x, y FROM loc_week_count_reshape"
+    dframe = pd.read_sql(sql, connection).sort_index()
     print("Data loded.")
 
     print("Building data structure…")
-    dset = {}
-    for i in range(len(data)):
-        row = data.iloc[i]
-        dset[(row.x, row.y)] = json.loads(row.usr_count_json)
-
-    dframe = pd.DataFrame.from_dict(dset, orient='index')
-    location = pd.DataFrame.from_records([row for row in dframe.index])
-    location.columns = ("x", "y")
+    dframe = dframe.assign(location=lambda X: list(zip(X.x, X.y)))
+    dframe['usr_count_normalize'] = dframe['usr_count_normalize'].map(lambda row: json.loads(row))
+    dframe.sort_values("location")
+    location = dframe.location
     print("Data structure builded.")
 
     # with open(os.path.join(__location__, "cluster_centers.json"), 'r') as f:
     #     _, cluster_centers = json.load(f)
 
     print("Preprocessing data…")
-    X = sklpp.maxabs_scale(np.matrix(dframe, dtype=np.double), copy=False)
-    sample_mask = np.random.choice(X.shape[0], size=1000, replace=False)
-    X_sample = X[sample_mask, :]
+    X = sklpp.maxabs_scale(np.asfarray(dframe['usr_count_normalize'].values.tolist()), copy=False)  # type: np.ndarray
+    # sample_mask = np.random.choice(X.shape[0], size=1000, replace=False)
+    with open(os.path.join(__location__, "middle_dump.json"), "r") as f:
+        middle_dump = json.load(f)  # type: tg.Dict["mask", "centers"]
+        sample_mask = np.array(middle_dump["mask"])
+    # X_sample = X[sample_mask, :]
     print("Data preprocessed.")
 
-    print("Affinity propagation model fitting…")
+    print("Birch model fitting…")
     t0 = time.time()
-    ap = sklc.AffinityPropagation(damping=.99, max_iter=10000).fit(X_sample)
-    cluster_centers = ap.cluster_centers_indices_
+    birch = sklc.Birch(threshold=0.28)
+    birch.fit(X)
+    cluster_centers = birch.subcluster_centers_
+    cluster_labels = birch.subcluster_labels_
+    labels = birch.labels_
     c = len(cluster_centers)
     t1 = time.time()
-    print("Affinity propagation model fitted.")
+    print("Birch model fitted.")
     print("%d cluster centers dectected." % c)
     print("Total time: %.2f" % (t1 - t0))
 
-    ap_result = ap.predict(X)
-    init_matrix = np.fromfunction(lambda c, n: ap_result[n] == c, (c, X.shape[0]), dtype=int)
+    # dump middle result
+    with open(os.path.join(__location__, "middle_dump.json"), 'w') as f:
+        json.dump({'mask': sample_mask, 'centers': cluster_centers}, f, cls=NumpyAndPandasEncoder)
 
-    # print("DBSCAN propagation model fitting…")
-    # t0 = time.time()
-    # ap = sklc.DBSCAN().fit(X)
-    # # result = ap.predict(X)
-    # cluster_centers = ap.core_sample_indices_
-    # c = len(cluster_centers)
-    # t1 = time.time()
-    # print("DBSCAN propagation model fitted.")
-    # print("%d cluster centers dectected." % c)
-    # print("Total time: %.2f" % (t1 - t0))
+    m = fuzzifier_determ(X.shape[1], X.shape[0])
+
+    # use Fuzzy c-means predict to build the initial_matrix
+    # cmeans accept (D, N) rather than (N, D)
+    # pdb.set_trace()
+    u, u0, d, jm, p, fpc = skfc.cmeans_predict(X.T, cluster_centers, m, 1e-3, 100)  # in the function will rotate X WTF
 
     print("Fuzzy c-means model fitting…")
     t2 = time.time()
-    # centers = np.matrix(X[sample_mask[cluster_centers, :]])
-    m = fuzzifier_determ(X.shape[1], X.shape[0])
-    fcm = {}
-    fcm['cntr'], fcm['u'], fcm['u0'], fcm['d'], fcm['jm'], fcm['p'], fcm['fpc'] = skfc.cmeans(X.T, c, m, 1e-3, 100, init_matrix)  # cmeans accept (D, N) rather than (N, D)
+    cntr, u, u0, d, jm, p, fpc = skfc.cmeans(X.T, c, m, 1e-3, 100, u)
     t3 = time.time()
     print("Fuzzy c-means model fitted.")
     print("Total time: %.2f" % (t3 - t2))
-    # kmeans = sklc.MiniBatchKMeans(n_clusters=c, init=centers, max_iter=1e8)
-    # result = kmeans.fit_predict(dframe)
 
     print("Plotting result…")
-    result = fcm['u'].T.argmax(axis=1)
+    result = u.T.argmax(axis=1)
     color = (result + 1) / (c + 1)
     rgb_color = [gray2rgb(x) for x in color]
     ax = plt.subplot(1, 1, 1)
@@ -134,10 +129,10 @@ def main() -> None:
     ax.set_xlim(115.8, 116.9)
     ax.set_ylim(39.6, 40.3)
     Voronoi.voronoi(location, color_set=pd.DataFrame.from_records(rgb_color), target_axes=ax, sample_limit=0, show=False)
+    print("Result plotted.")
     plt.show()
     # handles = [mpatches.Patch(color=gray2rgb((i + 1) / (c + 1)), label='cluster %d' % (i + 1)) for i in range(c)]
     # ax.legend(handles=handles, loc=0, borderaxespad=0.0)
-    print("Result plotted.")
 
     # print("Dumping result…")
     # with open(os.path.join(__location__, 'cluster_centers.json'), 'w', encoding='utf_8') as f:
