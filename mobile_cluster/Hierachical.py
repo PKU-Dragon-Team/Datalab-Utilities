b97a7e20a536aa51d4206fdcacf5e09ec3f1d3bd
SLAPaper <slapaper.pku@gmail.com>
2016-05-15 01:10:58 +0800

    feat: add Last Local Minimum Cluster

--- mobile_cluster/Hierachical.py
+++ mobile_cluster/Hierachical.py
@@ -1,37 +1,39 @@
 import numpy as np
 import typing as tg
 import scipy.spatial.distance as spsd
+import scipy.signal as spsn
 import heapq as hq
 
 
-def SimpleHierachicalCluster(X: np.ndarray, weight: tg.Union[np.ndarray]=None) -> np.ndarray:
+def SimpleHierachicalCluster(X: np.ndarray, weight: tg.Optional[np.ndarray]=None) -> np.ndarray:
     """My version of Hierachical Clustering, processing small amount of samples and give easily judgement of the hierachical tree
     Running time: O(N^2*D), N is n_samples, D is n_features
     """
     n_samples, n_features = X.shape
 
     if weight is None:
-        weights = np.ones(n_samples)
+        weights = np.ones(n_samples, dtype=int)
     else:
         weights = weight.copy()
 
-    output = np.zeros((n_samples - 1, 4))  # each row: (index1: int, index2:int, index_new:int, distance:float)
+    hierachical = np.zeros((n_samples - 1, 3), dtype=int)  # each row: (index1: int, index2:int, index_new:int)
+    distances = np.zeros(n_samples - 1)
     output_index = 0
 
     nodes = X.copy()
     remaining_indexes = set(range(n_samples))
-    distances = []
+    distance_heap = []
     calculated = set()
     for i in remaining_indexes:
         calculated.add(i)
         for j in remaining_indexes - calculated:
-            hq.heappush(distances, (spsd.euclidean(nodes[i], nodes[j]), i, j))
+            hq.heappush(distance_heap, (spsd.euclidean(nodes[i], nodes[j]), i, j))
     # now go with clustering
     while len(remaining_indexes) > 1:
         # drop merged ones
-        min_d, index1, index2 = hq.heappop(distances)
+        min_d, index1, index2 = hq.heappop(distance_heap)
         while not (index1 in remaining_indexes and index2 in remaining_indexes):
-            min_d, index1, index2 = hq.heappop(distances)
+            min_d, index1, index2 = hq.heappop(distance_heap)
 
         centroid = (weights[index1] * nodes[index1] + weights[index2] * nodes[index2]) / (weights[index1] + weights[index2])
         # now new centroid comes, drop i and j, and calculate new distances
@@ -39,13 +41,28 @@ def SimpleHierachicalCluster(X: np.ndarray, weight: tg.Union[np.ndarray]=None) -
         remaining_indexes.remove(index2)
         index_new = nodes.shape[0]
         for i in remaining_indexes:
-            hq.heappush(distances, (spsd.euclidean(nodes[i], centroid), i, index_new))
+            hq.heappush(distance_heap, (spsd.euclidean(nodes[i], centroid), i, index_new))
 
         remaining_indexes.add(index_new)
         weights = np.hstack((weights, weights[index1] + weights[index2]))
         nodes = np.vstack((nodes, centroid))
         # forming output
-        output[output_index] = (index1, index2, index_new, min_d)
+        hierachical[output_index] = (index1, index2, index_new)
+        distances[output_index] = min_d
         output_index += 1
 
-    return output, nodes, weights
+    return hierachical, distances, nodes, weights
+
+
+def LastLocalMinimumCluster(X: np.ndarray, weight: tg.Optional[np.ndarray]=None) -> tg.Tuple[np.ndarray, np.ndarray]:
+    n_samples, n_features = X.shape
+    hierachical, distances, nodes, weights = SimpleHierachicalCluster(X, weight)
+    # find local mimimums
+    extrema = spsn.argrelmin(distances)  # type: np.ndarray
+    last_local_minimum = extrema[0][len(extrema[0]) - 1]
+
+    merged_nodes = set(hierachical[:last_local_minimum, 0:2].flat)
+    cluster_nodes = set(range(len(nodes)))
+    cluster_centers = cluster_nodes - merged_nodes  # nodes that is not merged will be cluster_centers
+
+    return nodes[list(cluster_centers)], weights[list(cluster_centers)]
