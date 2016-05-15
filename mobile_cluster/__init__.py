6350c50d44a57291a2f8c80c4004fee418c8b850
SLAPaper <slapaper.pku@gmail.com>
2016-05-16 03:36:53 +0800

    feat: add dataloder

--- mobile_cluster/__init__.py
+++ mobile_cluster/__init__.py
@@ -7,6 +7,7 @@ import typing as tg
 
 import numpy as np
 import pandas as pd
+import pymysql
 
 
 class NumpyAndPandasEncoder(json.JSONEncoder):
@@ -35,3 +36,15 @@ def fuzzifier_determ(D: int, N: int) -> float:
     """The function to determ the fuzzifier paramter of fuzzy c-means
     """
     return 1 + (1418 / N + 22.05) * D**(-2) + (12.33 / N + 0.243) * D**(-0.0406 * math.log(N) - 0.1134)
+
+
+def dataloder(database_conf: tg.Dict, select_sql: str) -> pd.DataFrame:
+    HOST = database_conf['host']
+    USER = database_conf['user']
+    PASS = database_conf['pass']
+    DATABASE = database_conf['database']
+    CHARSET = database_conf['charset']
+    connection = pymysql.connect(host=HOST, user=USER, password=PASS, db=DATABASE, charset=CHARSET, cursorclass=pymysql.cursors.DictCursor)
+    dframe = pd.read_sql(select_sql, connection)
+    connection.close()
+    return dframe
