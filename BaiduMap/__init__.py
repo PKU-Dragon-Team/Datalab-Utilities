9cd69dbad4bb7628611efaca33a723c8f5713fd6
SLAPaper <slapaper.pku@gmail.com>
2016-05-17 23:54:43 +0800

    refactor: update BaiduMap

--- BaiduMap/__init__.py
+++ BaiduMap/__init__.py
@@ -1,21 +1,13 @@
-import png, numpy
-import matplotlib.pyplot as plt
-
-import json, urllib.request, collections.abc, os, sys
-from urllib.parse import quote_plus
+import collections.abc
+import json
+import typing as tg
+import urllib.request
 from collections import OrderedDict
+from urllib.parse import quote_plus
 
-AK = None
-SERVER_URL = None
-
-__location__ = os.path.join(os.getcwd(), os.path.dirname(os.path.realpath(__file__)))
-
-with open(os.path.join(__location__, 'config.json'), 'r') as config:
-    x = json.load(config)
-    AK = x['ak']
-    SERVER_URL = x['server']
-
-BASE_URL = "%s?ak=%s" % (SERVER_URL, AK)
+import matplotlib.pyplot as plt
+import numpy as np
+import png
 
 
 class URLBuilder:
@@ -50,41 +42,53 @@ class URLBuilder:
         return self.__url
 
 
-def buildURL(width=None, height=None, certer=[], zoom=None, copyright=1, scale=2, bbox=[], markers=[], markerStyles=[], labels=[], labelStyles=[], paths=[], pathStyles=[]):
-
+def buildURL(AK: str, SERVER_URL: str, width: tg.Optional[int]=None, height: tg.Optional[int]=None, center: tg.Union[str, tg.List]=[], zoom: tg.Optional[int]=None, copyright: int=1, scale: int=2, bbox: tg.Tuple[float, float, float, float]=[], markers=[], markerStyles=[], labels=[], labelStyles=[], paths=[], pathStyles=[]):
+    BASE_URL = "%s?ak=%s" % (SERVER_URL, AK)
     url = URLBuilder(BASE_URL)
     if width:
         url.addParam('width', quote_plus(str(width)))
+
     if height:
         url.addParam('height', quote_plus(str(height)))
-    if certer:
+
+    if center:
         if isinstance(center, str):
             url.addParam('center', quote_plus(center))
         elif isinstance(center, collections.abc.Sequence):
             url.addParam('center', quote_plus('%f,%f' % center))
+
     if zoom:
         url.addParam('zoom', quote_plus(str(zoom)))
+
     if copyright:
         url.addParam('copyright', quote_plus(str(copyright)))
+
     if scale:
         url.addParam('scale', quote_plus(str(scale)))
+
     if bbox:
         url.addParam('bbox', quote_plus('%f,%f,%f,%f' % bbox))
+
     if markers:
         pass
         # not implemented
+
     if markerStyles:
         pass
         # not implemented
+
     if labels:
         pass
         # not implemented
+
     if labelStyles:
         pass
         # not implemented
+
     if paths:
         pass
         # not implemented
+
     if pathStyles:
         pass
         # not implemented
@@ -92,7 +96,7 @@ def buildURL(width=None, height=None, certer=[], zoom=None, copyright=1, scale=2
     return url.generateURL()
 
 
-def fetchImage(url: str) -> numpy.matrix:
+def fetchImage(url: str) -> np.matrix:
     r = png.Reader(file=urllib.request.urlopen(url))
     data = r.asFloat()
     column_count = data[0]
@@ -100,13 +104,13 @@ def fetchImage(url: str) -> numpy.matrix:
     pngdata = data[2]
     plane_count = data[3]['planes']
 
-    image_2d = numpy.vstack(map(numpy.float_, pngdata))
-    image_3d = numpy.reshape(image_2d, (row_count, column_count, plane_count))
+    image_2d = np.vstack(map(np.float_, pngdata))
+    image_3d = np.reshape(image_2d, (row_count, column_count, plane_count))
 
     return image_3d
 
 
-def plotMap(image: numpy.matrix, alpha=1, show=False):
+def plotMap(image: np.matrix, alpha=1, show=False):
     # TODO: 透明、对齐
     plt.imshow(image, alpha=alpha)
     if show:
